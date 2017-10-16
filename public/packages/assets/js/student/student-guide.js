6c04efc3ae0ab86a833cb49632e82db5c7d69dff
Tolu29 <jonathan.garcia@curiosity.com.mx>
2017-10-16 13:58:48 -0500

    cambios

--- public/packages/assets/js/student/student-guide.js
+++ public/packages/assets/js/student/student-guide.js
@@ -135,6 +135,430 @@
         no: "No",
       },
       correctAnswer: "d"
+    },
+    {
+      question: "Me lo pasaría bien recibiendo mensajes telefónicos en una oficina ?",
+      answers: {
+        si: "Si",
+        no: "No",
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "Me gustaría encargarme de una caja registradora ?",
+      answers: {
+        si: "Si",
+        no: "No",
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "Me gustaría cuidar a personas enfermas –o animales heridos ?",
+      answers: {
+        si: "Si",
+        no: "No",
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "Me interesa conocer a personas que se dedican a la proyección y construcción de obras?",
+      answers: {
+        si: "Si",
+        no: "No",
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "Me interesa hacer un trabajo de investigación sobre la historia de la música o de la pintura?",
+      answers: {
+        si: "Si",
+        no: "No",
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "Me ofrecería a arreglar un reloj estropeado?",
+      answers: {
+        si: "Si",
+        no: "No",
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "Me gustaría dedicarme a actividades relacionadas con el dibujo, la pintura, la escultura?",
+      answers: {
+        si: "Si",
+        no: "No",
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "Prefiero actividades relacionadas con las matemáticas?",
+      answers: {
+        si: "Si",
+        no: "No",
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "Me gustaría ayudar a un labrador a cuidar las cosechas?",
+      answers: {
+        si: "Si",
+        no: "No",
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "Me gustaría vender en una tienda toda clase de productos?",
+      answers: {
+        si: "Si",
+        no: "No",
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "Me gustaría participar en un concurso literario?",
+      answers: {
+        si: "Si",
+        no: "No",
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "Me interesa un trabajo de tipo administrativo?",
+      answers: {
+        si: "Si",
+        no: "No",
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "Me gustaría dedicarme a interceder en conflictos, litigios, juicios de personas, sociedades, etc?",
+      answers: {
+        si: "Si",
+        no: "No",
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "Me interesa investigar los efectos negativos de la comunicación?",
+      answers: {
+        si: "Si",
+        no: "No",
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "Me gusta actuar, cantar para los demás; en definitiva, todo lo relacionado con el mundo del espectáculo?",
+      answers: {
+        si: "Si",
+        no: "No",
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "Me gustaría colaborar en las tareas de una ONG?",
+      answers: {
+        si: "Si",
+        no: "No",
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "Me lo pasaría bien dirigiendo un programa de radio?",
+      answers: {
+        si: "Si",
+        no: "No",
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "Me presentaría a un concurso de fotografía?",
+      answers: {
+        si: "Si",
+        no: "No",
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "Me interesa conocer el funcionamiento de los coches?",
+      answers: {
+        si: "Si",
+        no: "No",
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "Me compraría herramientas para realizar trabajos de bricolaje?",
+      answers: {
+        si: "Si",
+        no: "No",
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "Me gustaría conocer la maquinaria y las herramientas que se utilizan en el campo?",
+      answers: {
+        si: "Si",
+        no: "No",
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "Me interesa la política?",
+      answers: {
+        si: "Si",
+        no: "No",
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "Me gustaría ser monitor de un centro recreativo?",
+      answers: {
+        si: "Si",
+        no: "No",
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "Me gustaría formar parte de una asociación de defensa forestal?",
+      answers: {
+        si: "Si",
+        no: "No",
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "Me preocupan las cuestiones sociales: las personas mayores, la pobreza, los problemas del barrio, etc?",
+      answers: {
+        si: "Si",
+        no: "No",
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "Me sentiría satisfecho trabajando en un banco?",
+      answers: {
+        si: "Si",
+        no: "No",
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "Me gusta llevar la iniciativa en las conversaciones?",
+      answers: {
+        si: "Si",
+        no: "No",
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "Me ofrecería a realizar un dibujo para la revista del centro?",
+      answers: {
+        si: "Si",
+        no: "No",
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "Me interesan los temas de estadística?",
+      answers: {
+        si: "Si",
+        no: "No",
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "Me apuntaría a un cursillo de socorrismo?",
+      answers: {
+        si: "Si",
+        no: "No",
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "Me divierte pasar trabajos al ordenador?",
+      answers: {
+        si: "Si",
+        no: "No",
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "Me gusta el mundo de la tecnología?",
+      answers: {
+        si: "Si",
+        no: "No",
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "Me gusta escuchar y ayudar a mis compañeros cuando lo necesitan?",
+      answers: {
+        si: "Si",
+        no: "No",
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "Me lo pasaría bien trabajando en un gimnasio?",
+      answers: {
+        si: "Si",
+        no: "No",
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "Asistiría a un cursillo de jardinería?",
+      answers: {
+        si: "Si",
+        no: "No",
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "Me gustaría colaborar en una investigación?",
+      answers: {
+        si: "Si",
+        no: "No",
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "Me ofrecería voluntario para ordenar fichas en un archivo?",
+      answers: {
+        si: "Si",
+        no: "No",
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "Me gustaría trabajar en un laboratorio haciendo experimentos?",
+      answers: {
+        si: "Si",
+        no: "No",
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "Me gustaría desarrollar actividades que requieran el uso de microscopios, telescopios, etc?",
+      answers: {
+        si: "Si",
+        no: "No",
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "Me gustaría trabajar en una agencia de viajes?",
+      answers: {
+        si: "Si",
+        no: "No",
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "Me dedicaría a vender y alquilar pisos, casas, etc?",
+      answers: {
+        si: "Si",
+        no: "No",
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "Me gustaría participar en la resolución de problemas científicos?",
+      answers: {
+        si: "Si",
+        no: "No",
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "Me gustaría ser monitor de una actividad deportiva?",
+      answers: {
+        si: "Si",
+        no: "No",
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "Dedicaría parte de mi tiempo a ayudar a encontrar trabajo a las personas que lo necesitaran ?",
+      answers: {
+        si: "Si",
+        no: "No",
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "Me gustaría clasificar las entradas y salidas de mercancías en un ordenador de una oficina?",
+      answers: {
+        si: "Si",
+        no: "No",
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "Me gustaría ser monitor de alta montaña, de esquí, etc?",
+      answers: {
+        si: "Si",
+        no: "No",
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "Me apasiona el mundo del motor?",
+      answers: {
+        si: "Si",
+        no: "No",
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "Me gustaría dedicarme a la cría de toda clase de animales?",
+      answers: {
+        si: "Si",
+        no: "No",
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "Me gustaría ser el responsable de mantenimiento de una gran empresa?",
+      answers: {
+        si: "Si",
+        no: "No",
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "Me gustaría ser entrevistador de una empresa de opinión?",
+      answers: {
+        si: "Si",
+        no: "No",
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "Me gustaría diseñar campañas publicitarias?",
+      answers: {
+        si: "Si",
+        no: "No",
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "Me gustaría trabajar en el desarrollo de nuevas energías?",
+      answers: {
+        si: "Si",
+        no: "No",
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "Me gustaría desarrollar actividades relacionadas con la bolsa o los seguros?",
+      answers: {
+        si: "Si",
+        no: "No",
+      },
+      correctAnswer: "d"
     }
   ];
 
