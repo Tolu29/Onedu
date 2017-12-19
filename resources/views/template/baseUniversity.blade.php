1511825b9a1966ecd98ae36b8de2aab2e193e2cd
Tolu <tolu@debian.Arlekin>
2017-12-19 16:35:47 -0600

    cambios

--- resources/views/template/baseUniversity.blade.php
+++ resources/views/template/baseUniversity.blade.php
@@ -46,7 +46,7 @@ class="fixed-sn white-skin backColor"
         </div>
         <!-- SideNav slide-out button -->
         <div class="float-left landOnedu">
-          <h2 class="textOnedu">ONEDU</h2>
+          <h2 class="textOnedu"><span style="color: #f4c945 ;">ON</span><span style="color: rgb(57, 156, 95);">E</span><span style="color: ;">D</span>U</h2>
           <a href="#" data-activates="slide-out" class="button-collapse"><i class="fa fa-bars  burguerColor"></i></a>
         </div>
 
